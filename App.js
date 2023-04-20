e05a4abad8430ae4f1f9c0c1e1817b757a1ff64e
pankaj <pankajsingh78968@gmail.com>
2023-04-20 18:31:05 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Navbaar from './Navbaar';
+import Jumbo from './Jumbo'
+import './Style.css';
+import Strips from './Strips';
+
+function App() {
+  return (
+    <>
+    <Navbaar></Navbaar>
+    <Jumbo>
+    </Jumbo>
+    <Strips></Strips>
+
+    </>
+
+  );
+}
+
+export default App;
