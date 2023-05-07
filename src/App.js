17ce8bf607ecfe18e56d235f9b0771d6791cd44b
pankaj <pankajsingh78968@gmail.com>
2023-05-07 23:51:06 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 
+import Navbaar from './Navbaar';
 import './Style.css';
 import Footer from './Footer'; 
 import React from 'react';
-import  Navbaar from './Navbaar'
-import Contact from './Contact';
-import Services from './Services';
-import Homepage from './Homepage';
-import About from './About';
 export default function App()
 {
 return (  
@@ -14,10 +10,6 @@ return (
 
 
     <Navbaar></Navbaar>
-    <Contact></Contact>
-<Services></Services>
-<Homepage></Homepage>
-<About></About>
 <Footer>
  </Footer>
 
