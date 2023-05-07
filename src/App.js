59a1eddab9fcbea73580f305e6f73e3b88a61e1c
pankaj <pankajsingh78968@gmail.com>
2023-05-07 13:06:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-// import logo from './logo.svg';
-// import './App.css';
+
 import Navbaar from './Navbaar';
-// import Jumbo from './Jumbo'
 import './Style.css';
-// import Strips from './Strips';
-import Footer from './Footer';
-// import Services from './Services' 
+import Footer from './Footer'; 
 import React from 'react';
-// import Contact from './Contact';
 // import { Route, Routes } from "react-router-dom";
   function App() {
   return (
